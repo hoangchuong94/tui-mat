83fc24c57478dcc01c8e7f6f7ad05a7f324a786d
hoangchuong94 <hoangchuong300494@gmail.com>
2025-02-19 21:01:39 +0700

    test auth with prisma accelerate

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import Image from 'next/image';
+import { auth } from '@/auth';
 
-export default function Home() {
-    return <></>;
+export default async function Home() {
+    const session = await auth();
+
+    if (!session) {
+        return <div>Not authenticated</div>;
+    }
+    return (
+        <main>
+            <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+                <div className="container">
+                    <pre>{JSON.stringify(session, null, 2)}</pre>
+                </div>
+            </div>
+        </main>
+    );
 }
