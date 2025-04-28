9331d03f11b47d3eb0ff7077d4725cfa202bb603
hoangchuong94 <hoangchuong300494@gmail.com>
2025-04-28 19:02:07 +0700

    fixed deploy

--- app/(admin)/dashboard/product/new/page.tsx
+++ app/(admin)/dashboard/product/new/page.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import CreateProductForm from '@/components/create-product-form';
-import { fetchDataCreateProductForm, fetchGenders } from '@/actions/create-product';
+import { fetchDataCreateProductForm } from '@/actions/create-product';
 
 export default async function NewProductPage() {
     const data = await fetchDataCreateProductForm();
-    const { success, error, data: dataG } = await fetchGenders();
-
-    if (!success) {
-        console.log(error);
-    } else {
-        console.log(JSON.stringify(dataG, null, 2));
-    }
-
     return <CreateProductForm dataCreateProduct={data} />;
 }
